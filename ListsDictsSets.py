3ab4cf5ff884238c9aad80d030a549d4ef4ee760
jonathanhuydieu <jonathanhuydieu@gmail.com>
2021-04-20 11:26:18 -0400

    Experimenting with lists, sets, and dictionaries as well as their methods

---
+++ ListsDictsSets.py
@@ -0,0 +1,17 @@
+import sys
+
+string = "This is a string"
+print("Hello, World!")
+print("the length of the string is " + str(len(string)))
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+brands = {"Dell", "Apple", "Microsoft", "Lenovo"}
+print(brands)
+more_brands = {"Nvidia", "Niantic", "Asus", "Dell", 'Dell'}
+print(more_brands)
+
+brands.update(more_brands)
+print("After update:" + str(brands))
+
+print(more_brands.update(brands))
+print(str(more_brands))
