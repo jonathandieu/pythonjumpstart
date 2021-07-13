9f1f8f590c2874a7d9e4a308d8753501cd16ada5
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-07-13 00:19:35 -0400

    implemented global variable and function that can change the global variable

--- Global.py
+++ Global.py
@@ -1,1 +1,8 @@
+def change():
+    global variable1
+    variable1 = "changed value"
+    print(variable1)
+
 if __name__ == '__main__': # This statement prevents the script from running every time we import
+    variable1 = "initial value"
+    change()
